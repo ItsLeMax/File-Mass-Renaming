05b16d1baa23891f1ad095bf6e3d027c16524d32
ItsLeMax <lemaxyt1243@gmail.com>
2026-06-23 14:11:19 +0200

    docs: translate docs to english language

--- src/util/logger.js
+++ src/util/logger.js
@@ -3,30 +3,30 @@ const { COLORS } = require("../config/config.js");
 module.exports = {
 
     /**
-     * @description Schreibt eine Fehlermeldung in die Konsole
+     * @description Writes an error message into the console
      * @author ItsLeMax
-     * @param { String } message Nachricht für die Fehlermeldung
-     * @since 1.1.2 
+     * @param { String } message message for the error
+     * @since 1.1.2
      */
     error(message) {
         console.log(COLORS.error, message, COLORS.reset);
     },
 
     /**
-     * @description Schreibt eine Warnmeldung in die Konsole
+     * @description Writes a warning message into the console
      * @author ItsLeMax
-     * @param { String } message Nachricht für die Warnmeldung
-     * @since 1.1.2 
+     * @param { String } message message for the warning
+     * @since 1.1.2
      */
     warn(message) {
         console.log(COLORS.warning, message, COLORS.reset);
     },
 
     /**
-     * @description Schreibt eine Erfolgsmeldung in die Konsole
+     * @description Writes a success message into the console
      * @author ItsLeMax
-     * @param { String } message Nachricht für die Erfolgsmeldung
-     * @since 1.1.2 
+     * @param { String } message message of success
+     * @since 1.1.2
      */
     success(message) {
         console.log(COLORS.success, message, COLORS.reset);
