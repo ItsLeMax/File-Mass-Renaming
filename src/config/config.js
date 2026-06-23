94bbf485a86d8f8d132c16364fe5184583ac47ea
ItsLeMax <lemaxyt1243@gmail.com>
2026-06-23 13:24:42 +0200

    fix: update faulty keys

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    RECENTS_PATH = "../data/recent.json",
-    ARCHIVE_PATH = "../data/archive",
-    COLORS = {
+    RECENTS_PATH: "../data/recent.json",
+    ARCHIVE_PATH: "../data/archive",
+    COLORS: {
         success: "\x1b[32m",
         warning: "\x1b[33m",
         error: "\x1b[31m",
